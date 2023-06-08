7f5c3fe12b81c40b31e981ccfa788017f2b7b7ac
matteoserrazanetti <matteoserrazanetti@gmail.com>
2023-06-08 14:58:46 +0200

    multiple output files problem solved

--- selenomicrobiome_analysis.py
+++ selenomicrobiome_analysis.py
@@ -8,7 +8,7 @@ import multiprocessing
 help_msg="""This pipeline performs different kind of analysis on a dataset of metagenomes assemblies to characterize the 
 metagenomes (markers for Se usage and selenoprotein families)
 
-gives aboslute paths in all folder requirements
+give aboslute paths in all folder requirements
 
 example of command line:
 python3 selenomicrobiome_analysis.py -i input_directory -o output_directory -db database_file 
@@ -73,8 +73,7 @@ def main_function(arg_list):
   
   filename = arg_list[0] 
   opt = arg_list[1]
-  marker_list = arg_list[2]
-  fasta_ext = arg_list[3]
+  fasta_ext = arg_list[2]
   id=filename.split('/')[-1].split('.')[0]
 
   output_folder=opt['o']
@@ -85,7 +84,8 @@ def main_function(arg_list):
 
   if opt['S']:
     print('Starting selenoprofiles4 for '+id)
-    sp_fam_list=create_fam_list(opt['sp4_cfg'], marker_list)
+    marker_list=create_marker_list(opt['sp4_cfg'])
+    sp_fam_list=create_sp_fam_list(opt['sp4_cfg'], marker_list)
     selenoprofiles(filename, output_folder, opt['p'], id)
     count_selenoprotein_families(output_folder, id, marker_list, sp_fam_list)
   if opt['T']:
@@ -104,7 +104,7 @@ def main_function(arg_list):
 
 # important that file option needs to take a absoulte path for the file
 
-def create_fam_list(sp4_cfg, marker_list):
+def create_sp_fam_list(sp4_cfg, marker_list):
   sp_fam_list = []
   with open(sp4_cfg,'r') as infile:
     for line in infile:
@@ -117,8 +117,19 @@ def create_fam_list(sp4_cfg, marker_list):
               sp_fam_list.append(el)
   return(sp_fam_list)
 
+def create_marker_list(sp4_cfg):
+  marker_list = []
+  with open(sp4_cfg,'r') as infile:
+    for line in infile:
+      if line[0]=='f':
+        line=line.split()
+        if line[0]=='families_set.bact_markers':
+          l = line[2].split(',')
+          for el in l:
+            marker_list.append(el)
+  return(marker_list)
+
 def selenoprofiles(file, output_folder, profile, id):
-  #bbash('conda activate sp4')
   sp4_folder=output_folder+'/sp4'
   if not os.path.exists(sp4_folder):
     os.mkdir(sp4_folder)
@@ -137,57 +148,47 @@ def selenoprofiles(file, output_folder, profile, id):
   os.chdir(sp4_folder+'/'+id)
   bbash('selenoprofiles -o '+new_output_folder+' -t '+file+' -s '+id+' -p '+profile+' -no_splice '+gff_output+gtf_output)
   os.chdir(start_folder)
-  #bbash('conda deactivate')
 
 def count_selenoprotein_families(output_folder, id, marker_list, sp_fam_list):
-  for dir, subdirs, files in os.walk(output_folder+'/sp4'):
-    for file in files:
-      if file[:11]=='sp4_results' and file[-3:]=='gff':
-        with open(dir+'/'+file, 'r') as infile:
-          count=0
-          sp_fam_dict_partial = {}
-          sp_fam_dict_partial['seld_sec']=0
-          for el in sp_fam_list: #initialze the partial dictionary with selenoproteins genes
-            sp_fam_dict_partial[el]=0
-          for el in marker_list: #initialize the partial dictionary with marker genes (actually genes that do not use selenocysteine)
-            sp_fam_dict_partial[el]=0
-          for line in infile:
-            line = line.split()
-            sp_fam=line[8].split('.')[0]
-            if sp_fam[:4]=='Sec1' or sp_fam[:4]=='Sec2' or sp_fam[:4]=='Sec3': #correct the name of the family when is predicted with SeC
-              sp_fam = sp_fam.split(':')[1]
-              label=line[8].split('.')[-1]
-            if sp_fam == 'seld' and label == 'selenocysteine':
-              sp_fam_dict_partial['seld_sec']+=1
-            if (sp_fam in sp_fam_list and label == 'selenocysteine') or (sp_fam in marker_list and sp_fam != 'seld' and (label == 'homologue' or label == 'pseudo')) or (sp_fam == 'seld'):
-              print(sp_fam+'\t'+str(line[8].split('.')))
-              count+=1
-            if sp_fam not in sp_fam_dict_partial.keys():
-              sp_fam_dict_partial[sp_fam]=1
-            else:
-              sp_fam_dict_partial[sp_fam]+=1
+  with open(output_folder+'/sp4/'+id+'/'+id+'/sp4_results_'+id+'.gff', 'r') as infile:
+    count=0
+    sp_fam_dict_partial = {}
+    sp_fam_dict_partial['seld_sec']=0
+    for el in sp_fam_list: #initialze the partial dictionary with selenoproteins genes
+      sp_fam_dict_partial[el]=0
+    for el in marker_list: #initialize the partial dictionary with marker genes (actually genes that do not use selenocysteine)
+      sp_fam_dict_partial[el]=0
+    for line in infile:
+      line = line.split()
+      sp_fam = line[8].split('.')[0]
+      label = line[8].split('.')[0]
+      if sp_fam[:4]=='Sec1' or sp_fam[:4]=='Sec2' or sp_fam[:4]=='Sec3': #correct the name of the family when is predicted with SeC
+        sp_fam = sp_fam.split(':')[1]
+      if sp_fam == 'seld' and label == 'selenocysteine':
+        sp_fam_dict_partial['seld_sec']+=1
+      if (sp_fam in sp_fam_list and label == 'selenocysteine') or (sp_fam in marker_list and sp_fam != 'seld' and (label == 'homologue' or label == 'pseudo')) or (sp_fam == 'seld'):
+        #print(sp_fam+'\t'+str(line[8].split('.')))
+        count+=1
+      if sp_fam not in sp_fam_dict_partial.keys():
+        sp_fam_dict_partial[sp_fam]=1
+      else:
+        sp_fam_dict_partial[sp_fam]+=1
 
-        with open(dir+'/sp4_family_count_'+id+'.txt','w') as outfile:
-          outfile.write('  '+id+'\n')
-          outfile.write('total '+str(count)+'\n')
-          for x,y in sp_fam_dict_partial.items():
-            outfile.write(str(x)+' '+str(y)+'\n')
-  
-         
+  with open(output_folder+'/sp4/'+id+'/'+id+'/sp4_family_count_'+id+'.txt','w') as outfile:
+    outfile.write('  '+id+'\n')
+    outfile.write('total '+str(count)+'\n')
+    for x,y in sp_fam_dict_partial.items():
+      outfile.write(str(x)+' '+str(y)+'\n')
 
 #######################         Secmarker part     ####################################
 
 def secmarker(file, output_folder, id):
-  #bbash('conda activate secmarker')
-  #if not os.path.exists(output_folder+'/secmarker'):
-  #  os.mkdir(output_folder+'/secmarker')
   id=file.split('/')[-1].split('.')[0]
   if os.path.exists(output_folder+'/secmarker/'+id): # in this part i remove and re-create the folder if already present, this way i remove possible confusions
     shutil.rmtree(output_folder+'/secmarker/'+id) # remove     
   os.mkdir(output_folder+'/secmarker/'+id) # re-create
   print('secmarker -t '+file+' -o '+output_folder+'/secmarker/'+id+' -cpu 1')
   bbash('secmarker -t '+file+' -o '+output_folder+'/secmarker/'+id+' -cpu 1')
-  #bbash('conda deactivate')
 
 def secmarker_count(file, output_folder, id):
   with open(output_folder+'/secmarker/'+id+'/trnasec.gff','r') as infile:
@@ -200,9 +201,6 @@ def secmarker_count(file, output_folder, id):
 ########################       Infernal for rnaseP part    ###########################
 
 def infernal(file, output_folder, cm_file, id):
-  #bbash('conda activate infernal')
-  #if not os.path.exists(output_folder+'/infernal'):
-  #  os.mkdir(output_folder+'/infernal')
   if os.path.exists(output_folder+'/infernal/'+id): # in this part i remove and re-create the folder if already present, this way i remove possible confusions
     shutil.rmtree(output_folder+'/infernal/'+id) # remove     
   os.mkdir(output_folder+'/infernal/'+id) # re-create
@@ -212,7 +210,6 @@ def infernal(file, output_folder, cm_file, id):
   print(file)
   print('cmscan --rfam -E 1e-5 --cpu 1 --tblout '+output_folder+'/infernal/'+id+'/infernal_output_'+id+'.tbl '+cm_file+' '+file)
   bbash('cmscan --rfam -E 1e-5 --cpu 1 --tblout '+output_folder+'/infernal/'+id+'/infernal_output_'+id+'.tbl '+cm_file+' '+file)
-  #bbash('conda deactivate')
 
 def clean_infernal_output(output_folder, id):
   open(output_folder+'/infernal/'+id+'/infernal_output_'+id+'_cleaned.tbl', 'w').close() #empty the file before writing again on it
@@ -241,7 +238,6 @@ def clean_infernal_output(output_folder, id):
 def main(args={}):
 
   fasta_ext=['fna','fa','faa','frn','ffn','fasta']
-  marker_list = ['tigr04348', 'egtB', 'seld', 'yqeb', 'yqec', 'ybbb']
 
   print('Starting all the jobs')
 
@@ -280,7 +276,7 @@ def main(args={}):
   for dir, subdirs, files in os.walk(input_folder):
     for file in files:
       if file.split('.')[-1] in fasta_ext:
-        arg_list.append([dir+'/'+file,opt,marker_list,fasta_ext])
+        arg_list.append([dir+'/'+file,opt,fasta_ext])
 
 
   pool = multiprocessing.Pool(processes=cpu)
